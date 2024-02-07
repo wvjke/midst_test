1995cde986450d7792defaec27cdf4ee59cff731
unknown <ihor.wave@gmail.com>
2024-02-07 23:45:29 +0200

    changes

--- src/Components/AddTodo/AddTodo.tsx
+++ src/Components/AddTodo/AddTodo.tsx
@@ -1,53 +1,59 @@
-import { TodoInput, AddTodoWrapper } from "./AddTodo.styled";
+import { TodoInput, AddTodoWrapper } from './AddTodo.styled'
 // import {
 //     getTodos,
 //     todosUrlEndpoint as cacheKey,
 // } from "../../api/todosApi";
 // import useSWR from "swr";
-import { useRef } from "react";
+import { useRef } from 'react'
 // import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.min.css";
+import 'react-toastify/dist/ReactToastify.min.css'
 const AddTodo = () => {
+	const inputRef = useRef<HTMLInputElement>(null)
 
-    const inputRef = useRef<HTMLInputElement>(null);
+	// const handleAddTodo = async (e: React.FormEvent<HTMLFormElement>) => {
+	//     e.preventDefault();
+	//     console.log(inputRef.current?.value)
+	//     // try {
+	//     //     await addTodo({
+	//     //         text: inputRef.current
+	//     //             ? inputRef.current.value
+	//     //             : "nothing here",
+	//     //         completed: false,
+	//     //     });
+	//     //     toast.success("Todo added successfully");
+	//     //     mutate();
+	//     //     if (inputRef.current) {
+	//     //         inputRef.current.value = "";
+	//     //     }
+	//     // } catch (err) {
+	//     //     console.error(err);
+	//     // }
+	// };
 
-    // const handleAddTodo = async (e: React.FormEvent<HTMLFormElement>) => {
-    //     e.preventDefault();
-    //     console.log(inputRef.current?.value)
-    //     // try {
-    //     //     await addTodo({
-    //     //         text: inputRef.current
-    //     //             ? inputRef.current.value
-    //     //             : "nothing here",
-    //     //         completed: false,
-    //     //     });
-    //     //     toast.success("Todo added successfully");
-    //     //     mutate();
-    //     //     if (inputRef.current) {
-    //     //         inputRef.current.value = "";
-    //     //     }
-    //     // } catch (err) {
-    //     //     console.error(err);
-    //     // }
-    // };
+	function onSubmit(e:FormDataEvent) {
+		e.preventDefault();
+        let captcha = grecaptcha.getResponse();
+        console.log(captcha);
+	}
 
-    function onSubmit(token:any) {
-        console.log(token);
-    }
+	return (
+		<AddTodoWrapper>
+			<form
+				onSubmit={onSubmit}
+				style={{ display: 'flex', width: '100%', gap: '20px' }}
+				id='demo-form'
+				method='POST'
+			>
+				<TodoInput required ref={inputRef} />
+				<button
+					className='g-recaptcha'
+					data-sitekey='6LekMWspAAAAALwpi8hCrBZj1cMI1vyIE4aORfcA'
+				>
+					Submit
+				</button>
+			</form>
+		</AddTodoWrapper>
+	)
+}
 
-    console.log(onSubmit);
-
-    return (
-        <AddTodoWrapper>
-            <form
-                style={{ display: "flex", width: "100%", gap: "20px" }}
-                id="demo-form" method="POST"
-            >
-                <TodoInput required ref={inputRef} />
-            </form>
-            <button className="g-recaptcha" data-sitekey="6LekMWspAAAAALwpi8hCrBZj1cMI1vyIE4aORfcA" data-callback="onSubmit">Submit</button>
-        </AddTodoWrapper>
-    );
-};
-
-export default AddTodo;
+export default AddTodo
